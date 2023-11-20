c556e4a9078c1bf700cb7f628cd2a09612ec767f
Dmitriy Dultsev <tsukikohaya@gmail.com>
2023-11-20 20:58:09 +0300

    start project

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script lang="ts">
+export default {};
+</script>
+
+<template>
+  <div class="p-2">
+    <div>
+      <label for=""></label>
+      <input type="text" class="border-gray-500 border-[2px] rounded-md px-2" />
+    </div>
+  </div>
+</template>
