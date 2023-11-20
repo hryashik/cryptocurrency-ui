c9e511a956859b2ea481bc94115000a9d2144fa8
Dmitriy Dultsev <tsukikohaya@gmail.com>
2023-11-20 21:12:42 +0300

    add prettier-tailwind-plugin

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,21 @@ export default {};
 </script>
 
 <template>
-  <div class="p-2">
-    <div>
-      <label for=""></label>
-      <input type="text" class="border-gray-500 border-[2px] rounded-md px-2" />
-    </div>
-  </div>
+   <div class="p-2">
+      <div class="w-82">
+         <form @submit.prevent="" class="flex flex-col items-start">
+            <label for="ticker-input mb-2">Ticker</label>
+            <input
+               id="ticker-input"
+               type="text"
+               class="w-64 rounded-md border-indigo-300 px-2 shadow-xl focus:border-b-2 focus:outline-none"
+            />
+            <input
+               type="submit"
+               value="+  Add"
+               class="rounded-full bg-gray-500 px-4 py-2"
+            />
+         </form>
+      </div>
+   </div>
 </template>
